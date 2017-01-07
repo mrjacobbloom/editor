211ef66c94c5446707154dd86ffe5070ed974524
Jacob Bloom <mr.jacob.bloom@gmail.com>
2017-01-07 11:09:15 -0700

    make lines handled more robustly in JS

--- editor.js
+++ editor.js
@@ -19,22 +19,57 @@ function setSelect(inp, s, e) {
 function getParentLine(node) {;
   while(true) {
     if(node == document.body || !node.parentElement) return null;
-    if(node.matches('.line')) break;
+    console.log(node)
+    if(node.matches && node.matches('.line')) break;
     node = node.parentElement;
   }
   return node
 }
+function getCurrentLine() {
+  var selection = window.getSelection();
+  return getParentLine(selection.focusNode);
+}
 
 var editor = document.querySelector('#editor');
-editor.addEventListener('keydown', function(e) {
-  console.log(e)
-  if(e.code == 'ArrowUp') {
-    e.preventDefault();
-    if(e.shiftKey) {
-      // change selection
-    } else {
-      // if not top line, move cursor
-    }
-    return false;
+function Line(text, index) {
+  this.element = document.createElement('div');
+  this.element.classList.add('line');
+  this.element.setAttribute('contenteditable', '');
+  this.contentelement = document.createElement('div');
+  this.contentelement.classList.add('linecontent');
+  this.element.appendChild(this.contentelement);
+  this.contentelement.innerHTML = text || '';
+  if(index) {
+    
+  } else {
+    editor.appendChild(this.element);
   }
-});
+  
+  this.element.addEventListener('keydown', function(e) {
+    console.log(e)
+    switch (e.code) {
+      case 'Backspace': {
+        var line = getCurrentLine();
+        if(line.textContent.length === 0) {
+          editor.removeChild(line);
+          return false;
+        } else {
+          return true;
+        }
+      }
+      case 'ArrowUp': {
+        e.preventDefault();
+        if(e.shiftKey) {
+          // change selection
+        } else {
+          // if not top line, move cursor
+        }
+        return false;
+      }
+    }
+  });
+}
+
+new Line('foo');
+new Line('bar');
+new Line('baz');
