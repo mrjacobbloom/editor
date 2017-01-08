ca505581416905442033f56e4cd9a6d3ff80b494
Jacob Bloom <mr.jacob.bloom@gmail.com>
2017-01-08 14:38:59 -0700

    structure everything into a constructor for an entire editor document

--- editor.js
+++ editor.js
@@ -3,37 +3,7 @@ document.querySelector('#cogbutton').addEventListener('click', function() {
 });
 // also on blur, or on document.body when target isn't in the settingspanel
 
-var lines = [];
-
-function getTextElement(contentelement) {
-  if(contentelement.children.length != 1) {
-    let text = contentelement.textContent;
-    while (contentelement.firstChild) {
-      contentelement.removeChild(contentelement.firstChild);
-    }
-    let textnode = document.createTextNode(text);
-    contentelement.appendChild(textnode);
-    return textnode;
-  } else {
-    return contentelement.firstChild;
-  }
-}
-
-function setSelect(elem1, index1, elem2, index2) {
-  var range = document.createRange();
-  var sel = window.getSelection();
-  range.setStart(getTextElement(elem1), index1);
-  if(elem2) {
-    range.setEnd(getTextElement(elem2), index2);
-  } else {
-    range.collapse();
-  }
-  sel.removeAllRanges();
-  sel.addRange(range);
-}
-
-var editor = document.querySelector('#editor');
-function Line(text, index) {
+function Line(doc, text, index) {
   this.element = document.createElement('div');
   this.element.classList.add('line');
   this.element.setAttribute('contenteditable', '');
@@ -41,14 +11,14 @@ function Line(text, index) {
   this.contentelement.classList.add('linecontent');
   this.element.appendChild(this.contentelement);
   this.contentelement.appendChild(document.createTextNode(text || ''));
-  if(index !== undefined && lines[index]) {
-    editor.insertBefore(this.element, lines[index].element);
-    lines.splice(index, 0, this);
+  if(index !== undefined && doc.lines[index]) {
+    doc.editor.insertBefore(this.element, doc.lines[index].element);
+    doc.lines.splice(index, 0, this);
   } else {
-    editor.appendChild(this.element);
-    lines.push(this);
+    doc.editor.appendChild(this.element);
+    doc.lines.push(this);
   }
-  lineCountChanged();
+  doc.lineCountChanged();
   
   this.setText = function(_text) {
     this.contentelement.textContent = _text;
@@ -57,18 +27,18 @@ function Line(text, index) {
     return this.contentelement.textContent;
   }
   this.getNextLine = function() {
-    return lines[this.getIndex() + 1];
+    return doc.lines[this.getIndex() + 1];
   }
   this.getPreviousLine = function() {
-    return lines[this.getIndex() - 1];
+    return doc.lines[this.getIndex() - 1];
   }
   this.getIndex = function() {
-    return lines.indexOf(this);
+    return doc.lines.indexOf(this);
   }
   this.remove = function() {
-    editor.removeChild(this.element);
-    lines.splice(this.getIndex(), 1);
-    lineCountChanged();
+    doc.editor.removeChild(this.element);
+    doc.lines.splice(this.getIndex(), 1);
+    doc.lineCountChanged();
   }
   this.getLength = function() {
     return this.getText().length;
@@ -77,15 +47,15 @@ function Line(text, index) {
     return window.getSelection().anchorOffset;
   }
   
-  var self = this;
+  var line = this;
   
   this.element.addEventListener('paste', function(e) {
     if(e.clipboardData.types.indexOf('text/plain') > -1) {
         var data = e.clipboardData.getData('text/plain');
         //split at caret
         var oldcontent = [
-          self.getText().substring(0, self.getCaretPos()),
-          self.getText().substring(self.getCaretPos())
+          line.getText().substring(0, line.getCaretPos()),
+          line.getText().substring(line.getCaretPos())
         ];
         var split = data.split('\n');
         var caret = split[split.length - 1].length;
@@ -93,13 +63,13 @@ function Line(text, index) {
         console.log(split)
         split[0] = oldcontent[0] + split[0];
         split[split.length - 1] = split[split.length - 1] + oldcontent[1];
-        var line = self;
-        var index = self.getIndex();
-        line.setText(split[0]);
+        var currentline = line;
+        var index = line.getIndex();
+        currentline.setText(split[0]);
         for(let i = 1; i < split.length; i++) {
-          line = new Line(split[i], ++index);
+          currentline = new Line(doc, split[i], ++index);
         }
-        setSelect(line.contentelement, caret)
+        doc.setSelect(currentline.contentelement, caret)
     }
     e.preventDefault();
   });
@@ -109,16 +79,16 @@ function Line(text, index) {
     switch (e.code) {
       case 'Backspace': {
         // this logic should be different for selections, esp. multiline
-        if(self.getCaretPos() === 0) {
-          let prev = self.getPreviousLine();
+        if(line.getCaretPos() === 0) {
+          let prev = line.getPreviousLine();
           if(!prev) {
             e.preventDefault();
             return false;
           }
           let oldprevlength = prev.getLength();
-          prev.setText(prev.getText() + self.getText());
-          setSelect(prev.contentelement, oldprevlength);
-          self.remove();
+          prev.setText(prev.getText() + line.getText());
+          doc.setSelect(prev.contentelement, oldprevlength);
+          line.remove();
           e.preventDefault();
           return false;
         } else {
@@ -126,22 +96,22 @@ function Line(text, index) {
         }
       }
       case 'Enter': {
-        let text = self.getText();
-        let caret = self.getCaretPos();
-        self.setText(text.substring(0,caret));
-        let line = new Line( text.substring(caret) , self.getIndex() + 1);
-        setSelect(line.contentelement, 0);
+        let text = line.getText();
+        let caret = line.getCaretPos();
+        line.setText(text.substring(0,caret));
+        let newline = new Line(doc, text.substring(caret) , line.getIndex() + 1);
+        doc.setSelect(newline.contentelement, 0);
         e.preventDefault();
         return false;
       }
       case 'ArrowUp': {
-        if(self.getIndex() !== 0) {
-          var caret = self.getCaretPos();
-          var prev = self.getPreviousLine();
+        if(line.getIndex() !== 0) {
+          var caret = line.getCaretPos();
+          var prev = line.getPreviousLine();
           if(caret > prev.getLength()) {
-            setSelect(prev.contentelement, prev.getLength());
+            doc.setSelect(prev.contentelement, prev.getLength());
           } else {
-            setSelect(prev.contentelement, caret);
+            doc.setSelect(prev.contentelement, caret);
           }
           e.preventDefault();
           return false;
@@ -150,13 +120,13 @@ function Line(text, index) {
         }
       }
       case 'ArrowDown': {
-        if(self.getIndex() < lines.length - 1) {
-          var caret = self.getCaretPos();
-          var next = self.getNextLine();
+        if(line.getIndex() < doc.lines.length - 1) {
+          var caret = line.getCaretPos();
+          var next = line.getNextLine();
           if(caret > next.getLength()) {
-            setSelect(next.contentelement, next.getLength());
+            doc.setSelect(next.contentelement, next.getLength());
           } else {
-            setSelect(next.contentelement, caret);
+            doc.setSelect(next.contentelement, caret);
           }
           e.preventDefault();
           return false;
@@ -165,9 +135,9 @@ function Line(text, index) {
         }
       }
       case 'ArrowLeft': {
-        if(self.getIndex() !== 0 && self.getCaretPos() === 0) {
-          var prev = self.getPreviousLine();
-          setSelect(prev.contentelement, prev.getLength());
+        if(line.getIndex() !== 0 && line.getCaretPos() === 0) {
+          var prev = line.getPreviousLine();
+          doc.setSelect(prev.contentelement, prev.getLength());
           e.preventDefault();
           return false;
         } else {
@@ -175,9 +145,9 @@ function Line(text, index) {
         }
       }
       case 'ArrowRight': {
-        if((self.getIndex() < lines.length - 1) && self.getCaretPos() === self.getLength()) {
-          var next = self.getNextLine();
-          setSelect(next.contentelement, 0);
+        if((line.getIndex() < doc.lines.length - 1) && line.getCaretPos() === line.getLength()) {
+          var next = line.getNextLine();
+          doc.setSelect(next.contentelement, 0);
           e.preventDefault();
           return false;
         } else {
@@ -188,19 +158,64 @@ function Line(text, index) {
   });
 }
 
-var style = document.createElement('style');
-document.head.append(style);
-function lineCountChanged() {
-  // make sure line numbers are all the same width
-  if(lines.length) {
-    setTimeout(function() {
-      style.innerHTML = `.line::before { width: ${lines.length.toString().length + 1}ex; }`;
-    });
+function Document(text) {
+  this.lines = [];
+  
+  this.style = document.createElement('style');
+  document.head.append(this.style);
+  var doc = this;
+  this.lineCountChanged = function() {
+    // make sure line numbers are all the same width
+    if(doc.lines.length) {
+      setTimeout(function() {
+        doc.style.innerHTML = `.line::before { width: ${doc.lines.length.toString().length + 1}ex; }`;
+      });
+    }
   }
+  this.editor = document.querySelector('#editor');
+  
+  this.setSelect = function(elem1, index1, elem2, index2) {
+    
+    var getTextElement = function(contentelement) {
+      if(contentelement.children.length != 1) {
+        let text = contentelement.textContent;
+        while (contentelement.firstChild) {
+          contentelement.removeChild(contentelement.firstChild);
+        }
+        let textnode = document.createTextNode(text);
+        contentelement.appendChild(textnode);
+        return textnode;
+      } else {
+        return contentelement.firstChild;
+      }
+    }
+    
+    var range = document.createRange();
+    var sel = window.getSelection();
+    range.setStart(getTextElement(elem1), index1);
+    if(elem2) {
+      range.setEnd(getTextElement(elem2), index2);
+    } else {
+      range.collapse();
+    }
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
+  
+  if(text === undefined) text = '';
+  var split = text.split('\n');
+  for(let i = 0; i < split.length; i++) {
+    new Line(this, split[i]);
+  }
+  
+  this.setSelect(this.lines[0].contentelement, 0);
 }
 
 // Initialize
 (function() {
-  var line = new Line();
-  setSelect(line.contentelement, 0);
+  window.editor = new Document(`document.addEventListener('copy', function(e){
+    e.clipboardData.setData('text/plain', 'Hello, world!');
+    e.clipboardData.setData('text/html', '<b>Hello, world!</b>');
+    e.preventDefault(); // We want our data, not data from any selection, to be written to the clipboard
+});`);
 })();
