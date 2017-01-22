a8806e3ce497766e7a535c542303c4604189b48f
Jacob Bloom <mr.jacob.bloom@gmail.com>
2017-01-21 17:50:42 -0700

    implement open, new, and delete; design file open menu

--- files.js
+++ files.js
@@ -14,41 +14,50 @@ function FileSystem(doc) {
     this.docsData = [];
   }
   
-  if(this.docsData[0]) {
-    this.openFile = this.docsData[0];
-  } else {
-    this.openFile = {
+  var fs = this;
+  
+  this.open = function(file) {
+    title.value = file.title
+    titleDisabled.value = file.title + file.extension;
+    doc.setText(file.content);
+    fs.openFile = file;
+  }
+  this.save = function(force) {
+    if(force || fs.openFile.content != doc.getDocumentAsPlainText()
+      || fs.openFile.title != title.value) {
+      fs.openFile.content = doc.getDocumentAsPlainText();
+      fs.openFile.title = title.value;
+      localStorage.setItem('docsData', JSON.stringify(fs.docsData));
+      console.log('Document saved to localStorage');
+    } else {
+      console.log('Document not saved to localStorage since nothing changed');
+    }
+    console.log(fs.docsData);
+  }
+  this.new = function(text) {
+    var newfile = {
       'title': 'untitled',
       'extension': '.txt',
-      'content': 'document.addEventListener(\'copy\', function(e) {\n    e.clipboardData.setData(\'text/plain\', \'Hello, world!\');\n    e.clipboardData.setData(\'text/html\', \'<b>Hello, world!</b>\');\n    e.preventDefault(); // We want our data, not data from any selection, to be written to the clipboard\n});'
+      'content': text || ''
     };
-    this.docsData.push(this.openFile);
+    this.docsData.push(newfile);
+    this.open(newfile);
+    this.save(true);
   }
-  title.value = this.openFile.title
-  titleDisabled.value = this.openFile.title + this.openFile.extension;
-  doc.setText(this.openFile.content);
   
-  var fs = this;
+  if(this.docsData[0]) {
+    this.open(this.docsData[0]);
+  } else {
+    this.new('document.addEventListener(\'copy\', function(e) {\n    e.clipboardData.setData(\'text/plain\', \'Hello, world!\');\n    e.clipboardData.setData(\'text/html\', \'<b>Hello, world!</b>\');\n    e.preventDefault(); // We want our data, not data from any selection, to be written to the clipboard\n});');
+  }
   
   this.saveTimer = {
     interval: 3000,
     timer: null,
     reset: function() {
       if(fs.saveTimer.timer) fs.saveTimer.timer = clearTimeout(fs.saveTimer.timer);
-      fs.saveTimer.timer = setTimeout(fs.saveTimer.save, fs.saveTimer.interval);
+      fs.saveTimer.timer = setTimeout(fs.save, fs.saveTimer.interval);
       console.log(`Save timer reset to ${fs.saveTimer.interval}ms`);
-    },
-    save: function() {
-      if(fs.openFile.content != doc.getDocumentAsPlainText()
-        || fs.openFile.title != title.value + '.txt') {
-        fs.openFile.content = doc.getDocumentAsPlainText();
-        fs.openFile.title = title.value + '.txt';
-        localStorage.setItem('docsData', JSON.stringify(fs.docsData));
-        console.log('Document saved to localStorage');
-      } else {
-        console.log('Document not saved to localStorage since nothing changed');
-      }
-      console.log(fs.docsData);
     }
   }
   
@@ -56,6 +65,53 @@ function FileSystem(doc) {
   title.addEventListener('input', function(e) {
     titleDisabled.value = title.value + this.openFile.extension;
     fs.saveTimer.save();
+  });
+  
+  var file = document.querySelector('#file');
+  file.querySelector('.button').addEventListener('click', function(e) {
+    if(file.classList.contains('open')) {
+      file.classList.remove('open');
+    } else {
+      let menu = file.querySelector('.menu');
+      while(menu.firstChild) {
+        menu.removeChild(menu.firstChild);
+      }
+      for(let i = 0; i < fs.docsData.length; i++) {
+        let li = document.createElement('li');
+        let icon = document.createElement('img');
+        icon.setAttribute('src', 'icons/file.svg')
+        li.appendChild(icon);
+        li.appendChild(document.createTextNode(fs.docsData[i].title + fs.docsData[i].extension));
+        menu.appendChild(li);
+        li.addEventListener('click', function(e) {
+          fs.open(fs.docsData[i]);
+          file.classList.remove('open');
+        });
+      }
+      
+      let li = document.createElement('li');
+      let icon = document.createElement('img');
+      icon.setAttribute('src', 'icons/plus.svg')
+      li.appendChild(icon);
+      li.appendChild(document.createTextNode('New'));
+      menu.appendChild(li);
+      li.addEventListener('click', function(e) {
+        fs.new()
+        file.classList.remove('open');
+      });
+      
+      file.classList.add('open');
+    }
+  });
+  document.querySelector('#delete').addEventListener('click', function(e) {
+    var index = fs.docsData.indexOf(fs.openFile);
+    fs.docsData.splice(index, 1);
+    fs.save(true);
+    if(fs.docsData.length) {
+      fs.open(fs.docsData[0]);
+    } else {
+      fs.new();
+    }
   });
 }
 
